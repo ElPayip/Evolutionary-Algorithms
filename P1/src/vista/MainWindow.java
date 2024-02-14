3f50b5951bf64d18399b3ea3244b9228accb1269
Daniel Pérez García <91460252+daniperezg9@users.noreply.github.com>
2024-02-14 15:25:19 +0100

    Update MainWindow.java

--- P1/src/vista/MainWindow.java
+++ P1/src/vista/MainWindow.java
@@ -1,4 +1,19 @@
+import java.awt.BorderLayout;
 
-public class MainWindow {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class MainWindow extends JFrame {
+    Controller ctrl;
+
+    public MainWindow(Controller ctrl) {
+        super ("Programacion Evolutiva - Practica 1");
+        this.ctrl = ctrl;
+        initGUI();
+    }
+
+    private void initGUI() {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+    }
 }
