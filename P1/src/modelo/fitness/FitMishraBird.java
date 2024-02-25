7f149513697b67bd5284d736f76afaa1e405bb44
Daniel Pérez García <danper09@ucm.es>
2024-02-25 20:10:58 +0100

    Update FitMishraBird.java
    Me equivoqué en un paréntesis xd

--- P1/src/modelo/fitness/FitMishraBird.java
+++ P1/src/modelo/fitness/FitMishraBird.java
@@ -10,7 +10,7 @@ public class FitMishraBird implements Fitness<Double>{
 	public double eval(Individuo<Double> ind) {
 		List<Double> x = ind.getValores();
 		double fit = Math.sin(x.get(1)) * Math.exp(Math.pow((1-Math.cos(x.get(0))), 2));
-		fit += Math.cos(x.get(0) * Math.exp(Math.pow((1-Math.sin(x.get(1))), 2)));
+		fit += Math.cos(x.get(0)) * Math.exp(Math.pow((1-Math.sin(x.get(1))), 2));
 		fit += Math.pow(x.get(0) - x.get(1), 2);
 		return fit;
 	}
