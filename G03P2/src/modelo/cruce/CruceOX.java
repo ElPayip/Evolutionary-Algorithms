224855fe025c5a71b402b88d6dacbeb51300b2fd
Daniel Pérez García <danper09@ucm.es>
2024-03-12 20:44:00 +0100

    Creadas Clases de Cruces y Mutaciones
    Por fin he conseguido que funcione el proyecto en mi eclipse :D

---
+++ G03P2/src/modelo/cruce/CruceOX.java
@@ -0,0 +1,31 @@
+package modelo.cruce;
+
+import java.util.List;
+
+import modelo.genes.Gen;
+import utils.Pair;
+import vista.ConfigPanel.Option;
+
+public class CruceOX<T> implements Cruce<T> {
+
+	@Override
+	public Pair<List<Gen<T>>, List<Gen<T>>> cruzar(List<Gen<T>> crom1, List<Gen<T>> crom2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Cruce<T> clone() {
+		return new CruceOX<>();
+	}
+	
+	@Override
+	public <O> List<Option<O>> getExtraOpts() {
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "OX";
+	}
+}
---
+++ G03P2/src/modelo/mutacion/MutacionInsercion.java
@@ -0,0 +1,37 @@
+package modelo.mutacion;
+
+import java.util.List;
+
+import modelo.fitness.Fitness;
+import modelo.genes.Gen;
+import modelo.individuo.Individuo;
+import vista.ConfigPanel.Option;
+
+public class MutacionInsercion<T> implements Mutacion<T> {
+	
+	@Override
+	public void mutar(List<Gen<T>> crom) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public <O> List<Option<O>> getExtraOpts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void update(Fitness<T> fit, Individuo<T> ind) {
+		//TODO
+	}
+	
+	public Mutacion<T> clone(){
+		return new MutacionInsercion<>();
+	}
+
+	@Override
+	public String toString() {
+		return "Insercion";
+	}
+}
