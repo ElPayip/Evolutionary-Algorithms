dda54281f9415890bbd7a8f6971cef11d36f3d53
Daniel Pérez García <danper09@ucm.es>
2024-03-15 15:01:33 +0100

    Update CruceRecombinacionRutas.java

--- G03P2/src/modelo/cruce/CruceRecombinacionRutas.java
+++ G03P2/src/modelo/cruce/CruceRecombinacionRutas.java
@@ -18,7 +18,7 @@ public class CruceRecombinacionRutas<T> implements Cruce<T> {
 		List<Gen<T>> hijo1 = new ArrayList<>(crom1); Collections.fill(hijo1, null);
 		List<Gen<T>> hijo2 = new ArrayList<>(crom2); Collections.fill(hijo2, null);
 		
-		Map<Gen<T>, Set<Gen<T>>> conectividades = new HashMap<Gen<T>, Set<Gen<T>>>();
+		Map<Gen<T>, List<Gen<T>>> conectividades = new HashMap<Gen<T>, List<Gen<T>>>();
 		
 		for(int i = 0; i < crom1.size(); ++i) {
 			conectividades.put(crom1.get(i).clone(), null);
@@ -26,6 +26,20 @@ public class CruceRecombinacionRutas<T> implements Cruce<T> {
 			conectividades.get(crom1.get(i).clone()).add(crom1.get(i + 1 %crom1.size()));
 			conectividades.get(crom1.get(i).clone()).add(crom2.get(i - 1 %crom2.size()));
 			conectividades.get(crom1.get(i).clone()).add(crom2.get(i + 1 %crom2.size()));
+		}
+		
+		int i = 1;
+		hijo1.add(crom2.get(0).clone());
+		while(i < crom2.size()) {
+			Gen<T> seleccionado = null;
+			
+			for(int j = 0; j < conectividades.get(hijo1.get(i)).size(); ++j) {
+				if(conectividades.get(hijo1.get(i)).size() < conectividades.get(seleccionado).size() && !hijo1.contains(conectividades.get(hijo1.get(i)).get(j))){
+					seleccionado = conectividades.get(hijo1.get(i)).get(j);
+				}
+			}
+			hijo1.add(seleccionado);
+			++i;
 		}
 		
 		return new Pair<>(hijo1, hijo2);
@@ -43,6 +57,6 @@ public class CruceRecombinacionRutas<T> implements Cruce<T> {
 	
 	@Override
 	public String toString() {
-		return "Recombinación de Rutas";
+		return "ERX";
 	}
 }
