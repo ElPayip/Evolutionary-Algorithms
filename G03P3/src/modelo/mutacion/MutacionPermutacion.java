0871a451d37f76a56ffc8d2f1fb5d7cb2964a5c1
Daniel Pérez García <danper09@ucm.es>
2024-04-20 16:59:17 +0200

    Permutación (sin comprobar repetidos)
    Si encuentro como hacer que no se repitan las posiciones, lo cambio.

--- G03P3/src/modelo/mutacion/MutacionPermutacion.java
+++ G03P3/src/modelo/mutacion/MutacionPermutacion.java
@@ -1,15 +1,28 @@
 package modelo.mutacion;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import modelo.genes.Gen;
+import modelo.genes.GenNodo;
 import vista.ConfigPanel.Option;
 
 public class MutacionPermutacion<T> implements Mutacion<T> {
 
 	@Override
 	public void mutar(List<Gen<T>> crom) {
-		// TODO Auto-generated method stub
+		List<Gen<T>> funcs = new ArrayList<>();
+		for (Gen<T> g : crom)
+			if (!((GenNodo<T>) g).isTerminal()) funcs.add(g);
+		
+		int pos = new Random().nextInt(funcs.size());
+		GenNodo<T> funcion = (GenNodo<T>) funcs.get(pos);
+		
+		List<GenNodo<T>> hijos = funcion.getHijos();
+		
+		Collections.shuffle(hijos);
 	}
 	
 	@Override
